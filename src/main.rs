fec7db72580e968613be403aef4d32c3f4eb0869
Charles Barilleaux <chbarr@bandman.local>
2025-06-30 18:09:06 -0400

    Using if and let together

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     println!("# CHAPTER 3, SECTION 5: Control Flow\n");
 
-    println!("\n## `if` Statements");
 
-    let number=16;
+    println!("\n## `if` Statements");
+    
+    let number=17;
 
     if number<5 {
         println!("Condition was TRUE!");
@@ -14,6 +15,7 @@ fn main() {
     if number!=0 {
         println!("The number was nonzero");
     }
+
     if number % 4==0 {
         println!("Number is divisible by 4");
     } else if number % 3==0 {
@@ -23,5 +25,10 @@ fn main() {
     } else {
         println!("The number isn't divisible by 2, 3, or 4");
     }
+
+    println!("\n###if in a let statement");
+    let condition=true; //this was in the example. I started to play a bit. 
+    let newnumber=if (number%2!=0) {"Odd"} else {"Even"};
+    println!("The value is {newnumber}");
 
 }
